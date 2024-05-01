a38055178f800fd6d1f35b5eaf4fb21b3e03c637
fayinana <fekedeananiya@gmail.com>
2024-05-01 12:43:14 +0300

    aa

--- script.js
+++ script.js
@@ -19,29 +19,3 @@ exit.addEventListener('click',()=>{
   exit.style.display = 'none'
 
 });
-
-
-
-class Account{
-  constructor(owner,currency,pin){
-    this.owner = owner;
-    this.currency = currency;
-    this.pin = pin;
-    this.movements = []
-  }
-  deposit(val){
-    this.movements.push(val)
-  }
-  withdraw(value){
-    this.deposit(-value)
-  }
-}
-
-const acc1 = new Account('ananiya','etb',1010);
-console.log(acc1);
-acc1.deposit(100)
-acc1.deposit(100)
-acc1.deposit(100)
-acc1.withdraw(1000)
-acc1.withdraw(-1000)
-console.log(acc1);
