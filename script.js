87eeb7b80458b48840f7633854b8df32afb1b4ce
fayinana <fekedeananiya@gmail.com>
2024-05-09 11:16:06 +0300

    some modification

--- script.js
+++ script.js
@@ -19,3 +19,23 @@ exit.addEventListener('click',()=>{
   exit.style.display = 'none'
 
 });
+
+
+const navLists = document.querySelectorAll(".list a");
+navLists.forEach((navList) => {
+  navList.addEventListener("click", (e) => {
+    e.preventDefault();
+    navLists.forEach((nav) => {
+      nav.parentElement.classList.remove("active");
+    });
+    const id = e.currentTarget.getAttribute("href");
+    const element = document.querySelector(id);
+    navList.parentElement.classList.add("active");
+    // console.log(navList);
+    let position = element.offsetTop;
+    window.scrollTo({
+      left: 0,
+      top: position - 70,
+    });
+  });
+});
