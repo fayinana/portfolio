6cab7a0b20d6c01766dfb4879cb316d5eb00bfbc
fayinana <fekedeananiya@gmail.com>
2024-05-12 14:44:39 +0300

    some modification on the contact btn

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-const navBar = document.querySelector('.nav-inner');
-const menuBar = document.querySelector('.menu-nav');
-const exit = document.querySelector('.exit');
-const dateDom = document.querySelector('.date');
+const navBar = document.querySelector(".nav-inner");
+const menuBar = document.querySelector(".menu-nav");
+const exit = document.querySelector(".exit");
+const dateDom = document.querySelector(".date");
 
 const date = new Date();
 
-dateDom.innerHTML = date.getFullYear()
+dateDom.innerHTML = date.getFullYear();
 
-menuBar.addEventListener('click',()=>{
-  navBar.style.display = 'flex';
-  exit.style.display = 'flex'
-  menuBar.style.display = 'none'
+menuBar.addEventListener("click", () => {
+  navBar.style.display = "flex";
+  exit.style.display = "flex";
+  menuBar.style.display = "none";
 });
 
-exit.addEventListener('click',()=>{
-  navBar.style.display = 'none'
-  menuBar.style.display = 'flex'
-  exit.style.display = 'none'
-
+exit.addEventListener("click", () => {
+  navBar.style.display = "none";
+  menuBar.style.display = "flex";
+  exit.style.display = "none";
 });
-
 
 const navLists = document.querySelectorAll(".list a");
 navLists.forEach((navList) => {
